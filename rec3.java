5c0e94bf55e1b8167f4d9c1535e296a108b0d2a8
Kanish V <35183350+vangilikanish@users.noreply.github.com>
2021-02-09 17:09:20 -0500

    Update rec3.java

--- rec3.java
+++ rec3.java
@@ -12,13 +12,7 @@ boolean b = "true";
 
 **SOLUTION**
 
-int x = 5;
 
-double a = 0.25;
-
-char f = 'g';
-
-boolean b = true;
 
 */
 
@@ -42,46 +36,15 @@ c = (d + a)*b; // (8)
 b = a / a; // (9)
 b = a / c; // (1)
 b = (int) c / a; // (11)
-b = 52/5
-b = some double value 
-error
-
-b = (int)((int) c / a); // (11)
-b = (int)(52/5)
-
-b = (int)(c/a);
-
 d = c / a; // (12)
 
 **SOLUTION**
 
-(1) - b = 5, a = 5, c = 5.42
-(2) - d = 5.0, a = 5
-(3) - d = 5.42, c = 5.42
-(4) - error
-(5) - b = 5
-(6) - c = 10.0
-(7) - c = 30.42
-(8) - c = 52.1
-(9) - b = 1
-(10) - error
-(11) - error 
-(12) - 10.42
+
 
 */
 
 
 /*
-
 Whats you favorite food
-Kanish - french fries
-Atharva B. - ice cream
-Hasim - Buffalo Wings
-John Leng - mediteranean food   
-Alexander- Pizza
-Ketan - Chicken 
-Ethan - Chicken Parmesan Sub
-Shane - BBQ Steamed Pork Buns
-Anthony-  Garlic Parm Wings    
-Jingyan - fried chicken wings
 */
