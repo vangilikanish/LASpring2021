1cb0bf066a0eafe57a8b5c6ea35aa0e25d19c5c6
Kanish V <35183350+vangilikanish@users.noreply.github.com>
2021-02-15 20:58:37 -0500

    file ready for rec4

--- rec3.java
+++ rec3.java
@@ -12,7 +12,13 @@ boolean b = "true";
 
 **SOLUTION**
 
+int x = 5;
 
+double a = 2.0;
+
+char f = 'g';
+
+boolean b = true;
 
 */
 
@@ -34,17 +40,35 @@ c = a + b; // (6)
 c = d + a * b; // (7)
 c = (d + a)*b; // (8)
 b = a / a; // (9)
-b = a / c; // (1)
+b = a / c; // (10)
 b = (int) c / a; // (11)
 d = c / a; // (12)
 
 **SOLUTION**
 
-
+(1) b = 5;
+(2) d = 5.0;
+(3) d = 5.42;
+(4) b = error;
+(5) b = 5;
+(6) c = 10.0;
+(7) c = 29.42
+(8) c = 52.1
+(9) b = 1
+(10) b = error
+(11) b = error
+(12) d = 52.1/5
 
 */
 
-
 /*
 Whats you favorite food
+Kanish - French Fries
+Eli-  Pillsbury Cinnamon Rolls
+Shaan - Sushi
+Paul- skirt steak/sushi
+anuj- pizza
+Erick- steak
+Kieran - Burritos
+Bruce - Brazilian Food (Quiabo)
 */
---
+++ rec4.java
@@ -0,0 +1,53 @@
+public class rec4{
+    public static void main(String[] args){
+        /*
+        Can You Vote?
+        Problem: Write a program that reads in an age via an argument from the command line, and outputs whether or not you are allowed to vote at that age.
+        Hints: When are you allowed to vote? What data type is an age when it comes to voting? Is there a difference between 17.9 years old, 17 years old, and 18 years old?
+        */
+
+        //CODE HERE:
+
+
+        /*
+        Speeding Tickets
+        Problem: Assume you are about to give a ticket to a person that you caught speeding.
+        Get the speed as the input to your program via an argument from the command line, and output the correct fine:
+            Speed is less than or equal to 60mph: $0 fine
+            Speed is above 60 and less than or equal to 75: $100 fine
+            Speed is above 75 and less than or equal to 90: $200 fine
+            Speed is above 90: $500 fine
+        */
+
+        //CODE HERE:
+
+
+        /*
+        Horizontal Pyramid
+        Problem: Write a program that reads in a number, n, via an argument from the command line, and then creates a horizontal pyramid of height n
+        Example: height = 5:
+                            *
+                            **
+                            ***
+                            ****
+                            *****
+                            ****
+                            ***
+                            **
+                            *
+        */
+
+        //CODE HERE:
+
+
+        /*
+        Prime?
+        Problem: Write a program that will determine if a number is prime or not. If the number is prime, it will return true. If not, it will return false.
+        Hints: A prime number is a natural number greater than 1 that is not a product of two smaller natural numbers.
+        */
+
+        //CODE HERE:
+
+
+    }
+}
