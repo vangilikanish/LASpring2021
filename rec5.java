6de72ecb15994f8d0df2408daf1547626ff38ec8
Kanish V <35183350+vangilikanish@users.noreply.github.com>
2021-02-23 18:27:22 -0500

    Final push

--- rec5.java
+++ rec5.java
@@ -15,15 +15,27 @@ public class rec5 {
         //CODE:
         /*
         int[] arr = {1,7,3,6,3,2};
+        double sum = 0;
+        // write your code here
+        for(int i = 0; i < arr.length; i++)
+        {
+            sum += arr[i];
+        }
+        System.out.println(sum/arr.length);
+        */
+
+        /*
+        BR 1
+        int[] arr = {1,7,3,6,3,2};
         double sum = 0.0;
         for (int i = 0; i < arr.length; i++){
             sum = sum + arr[i];
         }
         System.out.println(sum/arr.length);
         */
-
         /*
 
+        /*
         Breakout Room 2
         Problem: Matrix 1’s and 0’s
         Write a program which takes a 2D array(this 2D array should be randomly filled with 1s and 0s), and prints:
@@ -43,8 +55,26 @@ public class rec5 {
         */
 
         //CODE:
-        //public static void main ( String[] args ) {
         /*
+        int[][] sample = {{1,0,1,0}, {1,1,0,1}, {1,0,0,1}, {0,0,1,0}};
+        int one = 0;
+        int zero = 0;
+        for(int i = 0; i < sample.length; i++ ) {
+            for(int j = 0; j < sample.length; j++ ){
+                if(sample[i][j] == 1){
+                    one += 1;
+                }
+                else{
+                    zero += 1;
+                }
+            }
+        }
+        System.out.println(one);
+        System.out.println(zero);
+        */
+
+        /*
+        BR 2
         int[][] arr = {{1,0,1,0}, {1,1,0,1}, {1,0,0,1}, {0,0,1,0}};
         int onesCount = 0;
         int zerosCount = 0;
@@ -60,9 +90,8 @@ public class rec5 {
         System.out.println("There were " + onesCount + " ones and " + zerosCount + " zeroes.");
         */
         
-        
         /*
-        Breakout Room 3
+        Breakout Room 1
         Problem: Nth Column of Matrix
         Write a program which takes a 2D integer array, arr, and an integer, n, and creates an array containing the nth column of arr.
         You may assume that the matrix has the same number of rows in each column, and that n is a valid column index.
@@ -82,8 +111,31 @@ public class rec5 {
         0
         */
 
-        //CODE: 
+        //CODE:
+        /* 
+        int[][] sample = {{1,0,1,0}, {1,1,0,1}, {1,0,0,1}};
+        int n = Integer.parseInt(args[0]);
+        //int[] newarray = new int[sample.length];
+        for (int i = 0; i < sample.length; i++)
+        {
+            System.out.println(sample[i][n]);
+        }
+        */
+
         /*
+        int[][] arr = {{1,0,1,0},{1,1,0,1},{1,0,0,1}};	 
+        int n = Integer.parseInt(args[0]);
+        int[] col = new int[arr.length]; 
+        for(int i = 0; i < arr.length; i++){ 
+            col[i] = arr[i][n];
+        }
+        for(int i = 0; i < col.length; i++){ 
+            System.out.println(col[i]);
+        }
+        */
+
+        /*
+        BR 3
         int n = Integer.parseInt(args[0]);
         int[][] arr = { {1,0,1,0}, {1,1,0,1}, {1,0,0,1} };
         int[] col = new int[arr.length];
@@ -94,21 +146,6 @@ public class rec5 {
             System.out.println(col[j] + ", ");
         }
         */
-
-        /*
-        Breakout Room 4
-        Problem: Array Concatenation
-        Write a program which takes two integer arrays, a and b, and creates an array which is a concatenated with b.
-        Sample arrays:
-        a = {1, 2, 3}
-        b = {4, 5, 6}
-        Output Example:
-        $ javac ArrayConcatenation.java
-        $ java ArrayConcatenation
-        1, 2, 3, 4, 5, 6,
-        */
-
-        //CODE:
         /*
         int[] a = new int[] {1, 2, 3};
         int[] b = new int[] {4, 5, 6};
@@ -125,7 +162,36 @@ public class rec5 {
         System.out.println(c);
         */
 
+
         /*
+        Breakout Room 2
+        Problem: Array Concatenation
+        Write a program which takes two integer arrays, a and b, and creates an array which is a concatenated with b.
+        Sample arrays:
+        a = {1, 2, 3}
+        b = {4, 5, 6}
+        Output Example:
+        $ javac ArrayConcatenation.java
+        $ java ArrayConcatenation
+        1, 2, 3, 4, 5, 6,
+        */
+
+        //CODE:
+        /*
+        int[] a = {1, 2, 3};
+        int[] b = {4, 5, 6};
+        int[] c = new int[a.length + b.length];
+        for (int i = 0; i < a.length; i++) 
+            c[i] = a[i];
+        for (int j = 0; j < b.length; j++)
+            c[j+a.length] = b[j];
+        for(int i = 0; i < a.length + b.length; i ++){
+            System.out.print(c[i]+ ",");
+        }
+        */
+        
+        /*
+        BR 4
         int[] a = {2,5,7,3};
         int[] b = {8,7,1,7};
         int finalSize = a.length + b.length;
@@ -151,6 +217,5 @@ public class rec5 {
         // for (int i = 0; i < c.length; i++) {
         //     System.out.println(c[0]);
         // }
-        
     }
 }
