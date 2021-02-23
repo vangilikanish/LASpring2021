5b78c3273712ba61da13b8fa8bbb49dcdbd5d683
Kanish V <35183350+vangilikanish@users.noreply.github.com>
2021-02-23 10:23:24 -0500

    create rec5.java

---
+++ rec5.java
@@ -0,0 +1,83 @@
+public class rec5 {
+    public static void main(String[] args){
+        /*
+        Breakout Room 1
+        Problem: Array Average
+        Write a program which computes the average of an array of integers.
+        Output Example: 
+        $ javac ArrayAverage.java
+        $ java ArrayAverage
+        3.667
+        //or sth similar
+
+        */
+
+        //CODE:
+        int[] arr = {1,7,3,6,3,2};
+        double sum = 0;
+        // write your code here
+        System.out.println(sum/arr.length);
+
+        /*
+        Breakout Room 2
+        Problem: Matrix 1’s and 0’s
+        Write a program which takes a 2D array(this 2D array should be randomly filled with 1s and 0s), and prints:
+        How many 1's are in the 2D 
+        array.
+        How many 0's are in the 2D
+        array.
+        Sample 2D array:
+        {1,0,1,0},
+        {1,1,0,1},
+        {1,0,0,1},
+        {0,0,1,0}
+        Output Example:
+        $ javac OneZeroArray.java
+        $ java OneZeroArray
+        There were 8 ones and 8 zeroes.
+        */
+
+        //CODE:
+
+
+        /*
+        Breakout Room 3
+        Problem: Nth Column of Matrix
+        Write a program which takes a 2D integer array, arr, and an integer, n, and creates an array containing the nth column of arr.
+        You may assume that the matrix has the same number of rows in each column, and that n is a valid column index.
+        Sample 2D array:
+        {1,0,1,0},
+        {1,1,0,1},
+        {1,0,0,1}
+        Output Example:
+        $ javac NthColumn.java
+        $ java NthColumn 0
+        1
+        1
+        1
+        $ java NthColumn 1
+        0
+        1
+        0
+        */
+
+        //CODE:
+
+        /*
+        Breakout Room 4
+        Problem: Array Concatenation
+        Write a program which takes two integer arrays, a and b, and creates an array which is a concatenated with b.
+        Sample arrays:
+        a = {1, 2, 3}
+        b = {4, 5, 6}
+        Output Example:
+        $ javac ArrayConcatenation.java
+        $ java ArrayConcatenation
+        1, 2, 3, 4, 5, 6,
+        */
+
+        //CODE:
+
+        
+    }
+}
