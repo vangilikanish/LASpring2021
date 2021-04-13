91ca4a4270e47038ef1f336fa65fb6986faeef94
Kanish V <35183350+vangilikanish@users.noreply.github.com>
2021-04-13 18:01:07 -0400

    done rec 23

--- rec11/rec11.java
+++ rec11/rec11.java
@@ -7,14 +7,6 @@ public class rec11 {
         (IE: if there are six strings, it will print all six strings once, and then print them all again).
         Ex: String[ ] arr = {“CS”, “111”, “Recitation”}
         
-        String[ ] arr = {“CS”, “111”, “Recitation”};
-        for(i=0; i<arr.length; i++){
-            System.out.println(arr[i]);
-        }
-        for(i=0; i<arr.length; i++){
-            System.out.println(arr[i]);
-        }
-
         You would print this:
         CS
         111
@@ -28,28 +20,20 @@ public class rec11 {
         Code:
         */
 
+        System.out.println("CS");
+        System.out.println("111");
+        System.out.println("Recitation");
+        System.out.println("CS");
+        System.out.println("111");
+        System.out.println("Recitation");
+ 
         /*
-        Group 2: What are the Big-O's of the following code snippets? Why?
-        int[] array = ... // Somehow create an array
-        for(int i = 0; i < array.length; i ++){
-            for(int j = 0; j < array.length; j ++){
-                System.out.println(array[i]);
-            }
-        }​
-        Answer: 
-
-        int[] array = ... // Create an array
-        for(int i = 0; i < array.length; i ++){
-            for(int j = 0; j < 10; j ++){
-                System.out.println(array[i]);
-            }
-        }
-        Answer:
+        Group 2:
 
         Complete the following method which would return the index of x in a given array using sequential search; if number is not found, return -1:
-        public static int sequentialSearch(int[] arr, int x)
 
         Trace the method above for finding the number 1 in [15, 2, 8, 1, 17, 5]:
+        
 
 
         How many total comparisons were made?: 
@@ -65,5 +49,14 @@ public class rec11 {
         What is the best case, worst case, and big O?:
         */
 
+    }
+
+    public static int sequentialSearch(int[] arr, int x){
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == x){
+                return i;
+            }
+        }
+        return -1;
     }
 }
