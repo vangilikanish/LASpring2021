7b16145571bb8913184fec29805f2203afc3caae
Kanish V <35183350+vangilikanish@users.noreply.github.com>
2021-04-27 17:10:52 -0400

    last one sec 26

---
+++ rec13/rec13.java
@@ -0,0 +1,20 @@
+public class rec13{
+    public static void main(String[] args){
+        /*
+        Problem 1: Write a recursive method, binarySearch, with the following method signature:
+        public static int binarySearch(int[] arr, int target, int left, int right);
+        */
+
+        /*
+        Problem 2: Write a recursive method, binarySearch, with the following method signature:
+        public static void insertionSort(int arr[])
+        */
+
+        /*
+        Problem 3: How does the Merge Sort algorithm work?
+        Trace MergeSort on the following array: [L,D,Z,V,G,O,W,B]
+        List the resulting array after each iteration of the mergesort algorithm. 
+        Indicate the number of character-to-character comparisons made for each call to merge
+        */
+    }
+}
